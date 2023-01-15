b161d15ce0800a0ecaacaf0f8af32e1bb591cb6d
Mitresh Lukhi <mitresh.22397@gmail.com>
2023-01-15 18:06:27 +0000

    first commit

---
+++ Forloop/Five.java
@@ -0,0 +1,12 @@
+package F.Forloop;
+
+public class Five {
+    public static void main(String[] args) {
+        for (int i = 1; i <=5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j+ " ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
---
+++ Forloop/Two.java
@@ -0,0 +1,11 @@
+package F.Forloop;
+
+public class Two {
+    public static void main(String[] args) {
+        int sum =0;
+        for (int i=1; i<=10; i++){
+            sum += i;
+        }
+        System.out.println("sum is :" + sum);
+    }
+}
